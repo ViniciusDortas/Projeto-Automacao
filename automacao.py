658be6f0772786f1a18318331b95a2b843957ac9
Vinicius Dortas <viniciusdortasgama@gmail.com>
2026-02-20 00:26:18 -0300

    calculo de ticket medio finalizado e adicionado ao .py

--- automacao.py
+++ automacao.py
@@ -3,22 +3,21 @@ import pandas as pd
 
 def carregar_dados(caminho):
     df = pd.read_excel(caminho)
+    df["Data"] = pd.to_datetime(df["Data"])
     df["Ano"] = df["Data"].dt.year
     return df
 
 
 def calcular_faturamento(df):
     faturamento_dia = (
-        df.groupby(["ID Loja", "Data"])["Valor Final"]
+        df.groupby(["ID Loja", "Data"], as_index=False)["Valor Final"]
         .sum()
-        .reset_index()
         .rename(columns={"Valor Final":"Faturamento Diario"})
     )
 
     faturamento_ano = (
-        df.groupby(["ID Loja", "Ano"])["Valor Final"]
+        df.groupby(["ID Loja", "Ano"], as_index=False)["Valor Final"]
         .sum()
-        .reset_index()
         .rename(columns={"Valor Final":"Faturamento Anual"})
     )
 
@@ -27,20 +26,46 @@ def calcular_faturamento(df):
 
 def calcular_variedade(df):
     variedade_itens_dia = (
-        df.groupby(["ID Loja", "Data"])["Produto"]
+        df.groupby(["ID Loja", "Data"], as_index=False)["Produto"]
         .nunique()
-        .reset_index()
         .rename(columns={"Produto":"Variedade Diaria"})
     )
 
     variedade_itens_ano = (
-        df.groupby(["ID Loja", "Ano"])["Produto"]
+        df.groupby(["ID Loja", "Ano"], as_index=False)["Produto"]
         .nunique()
-        .reset_index()
         .rename(columns={"Produto":"Variedade Anual"})
     )
 
     return variedade_itens_dia, variedade_itens_ano
+
+
+def calcular_ticket_medio(df):
+    ticket_medio_dia = (
+        df.groupby(["ID Loja", "Data"], as_index=False)[["Quantidade", "Valor Final"]]
+        .sum()
+        .assign(
+            Ticket_Medio_Dia=lambda x: (
+                x["Valor Final"] / x["Quantidade"].replace(0, pd.NA)
+            ).round(2)
+        )
+        .drop(columns=["Valor Final", "Quantidade"])
+        .rename(columns=lambda x: x.replace("_", " "))
+    )   
+
+    ticket_medio_ano = (    
+        df.groupby(["ID Loja", "Ano"], as_index=False)[["Quantidade", "Valor Final"]]
+        .sum()
+        .assign(
+            Ticket_Medio_Ano=lambda x: (
+                x["Valor Final"] / x["Quantidade"].replace(0, pd.NA)
+            ).round(2)
+        )
+        .drop(columns=["Valor Final", "Quantidade"])
+        .rename(columns=lambda x: x.replace("_", " "))
+    )
+
+    return ticket_medio_dia, ticket_medio_ano
 
 
 def main():
@@ -48,6 +73,7 @@ def main():
 
     faturamento_dia, faturamento_ano = calcular_faturamento(df)
     variedade_itens_dia, variedade_itens_ano = calcular_variedade(df)
+    ticket_medio_dia, ticket_medio_ano = calcular_ticket_medio(df)
 
     print("Processamento concluído")
 
