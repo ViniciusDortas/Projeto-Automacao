a9d63ccdb7f12aec7d0a5b39a2029588767c6c9d
Vinicius Dortas <viniciusdortasgama@gmail.com>
2026-02-20 22:46:07 -0300

    Adicionados os status de meta batida em cada metrica

--- automacao.py
+++ automacao.py
@@ -1,5 +1,21 @@
 import pandas as pd
+import numpy as np
 
+# ============
+# CONSTANTES
+# ============
+
+ARQUIVO_DADOS = r"Bases de Dados/Vendas.xlsx"
+
+FAT_META_DIA = 1000
+FAT_META_ANO = 1_650_000
+VAR_META_DIA = 4
+VAR_META_ANO = 120
+TICKET_META = 500
+
+# ===========
+# FUNÇÕES
+# ==========
 
 def carregar_dados(caminho):
     df = pd.read_excel(caminho)
@@ -68,14 +84,47 @@ def calcular_ticket_medio(df):
     return ticket_medio_dia, ticket_medio_ano
 
 
+def verificar_meta(df, valor, meta):
+    df = df.copy()
+    df["Bateu Meta"] = np.where(df[valor] >= meta, "Sim", "Não")
+    return df
+
+# ========
+# MAIN
+# ========
+
 def main():
-    df = carregar_dados(r"Bases de Dados/Vendas.xlsx")
+    df = carregar_dados(ARQUIVO_DADOS)
 
     faturamento_dia, faturamento_ano = calcular_faturamento(df)
     variedade_itens_dia, variedade_itens_ano = calcular_variedade(df)
     ticket_medio_dia, ticket_medio_ano = calcular_ticket_medio(df)
+    
+    lista_dfs = [
+        (faturamento_dia, "Faturamento Diario", FAT_META_DIA),
+        (faturamento_ano, "Faturamento Anual", FAT_META_ANO),
+        (variedade_itens_dia, "Variedade Diaria", VAR_META_DIA),
+        (variedade_itens_ano, "Variedade Anual", VAR_META_ANO),
+        (ticket_medio_dia, "Ticket Medio Dia", TICKET_META),
+        (ticket_medio_ano, "Ticket Medio Ano", TICKET_META)
+    ]
+
+    dfs_novos = [
+        verificar_meta(df, valor, meta)
+        for df, valor, meta in lista_dfs
+    ]
+
+    (
+        faturamento_dia,
+        faturamento_ano,
+        variedade_itens_dia,
+        variedade_itens_ano,
+        ticket_medio_dia,
+        ticket_medio_ano
+    ) = dfs_novos
 
     print("Processamento concluído")
+
 
 if __name__ == "__main__":
     main()
