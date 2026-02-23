7e49e80c244d081831cf0d0cfe4e022ae5d8d7f5
Vinicius Dortas <viniciusdortasgama@gmail.com>
2026-02-22 23:39:46 -0300

    calculadas as informações de todos os onepages

--- automacao.py
+++ automacao.py
@@ -5,7 +5,9 @@ import numpy as np
 # CONSTANTES
 # ============
 
-ARQUIVO_DADOS = r"Bases de Dados/Vendas.xlsx"
+ARQUIVO_VENDAS = r"Bases de Dados/Vendas.xlsx"
+ARQUIVO_EMAILS = r"Bases de Dados/Emails.csv"
+ARQUIVO_LOJAS = r"Bases de Dados/Lojas.csv"
 
 FAT_META_DIA = 1000
 FAT_META_ANO = 1_650_000
@@ -13,15 +15,26 @@ VAR_META_DIA = 4
 VAR_META_ANO = 120
 TICKET_META = 500
 
-# ===========
+# ==========
 # FUNÇÕES
 # ==========
 
-def carregar_dados(caminho):
-    df = pd.read_excel(caminho)
-    df["Data"] = pd.to_datetime(df["Data"])
-    df["Ano"] = df["Data"].dt.year
-    return df
+def carregar_dados(vendas, emails, lojas):
+    df_vendas = pd.read_excel(vendas)
+    df_vendas["Data"] = pd.to_datetime(df_vendas["Data"])
+    df_vendas["Ano"] = df_vendas["Data"].dt.year
+
+    df_emails = pd.read_csv(emails)
+    df_lojas = pd.read_csv(lojas, sep=";")
+
+    df_emails_lojas = df_emails.merge(
+        df_lojas,
+        on="Loja",
+        how="left"
+    )
+    df_emails_lojas["ID Loja"] = df_emails_lojas["ID Loja"].astype("Int64")
+
+    return df_vendas, df_emails_lojas
 
 
 def calcular_faturamento(df):
@@ -89,16 +102,47 @@ def verificar_meta(df, valor, meta):
     df["Bateu Meta"] = np.where(df[valor] >= meta, "Sim", "Não")
     return df
 
+
+def data_atual(df):
+
+    total_lojas = df["ID Loja"].nunique()
+
+    data_mais_recente = (
+        df
+        .groupby("Data")["ID Loja"]
+        .nunique()
+        .loc[lambda x: x == total_lojas]
+        .index
+        .max()
+    )
+    ano_atual = data_mais_recente.year
+
+    return data_mais_recente, ano_atual
+
+
+def filtrar_data_dfs(df, coluna, data):
+    return df[df[coluna] == data]
+
+
+def loc_onepage(df, id_loja, coluna):
+    if id_loja in df.index:
+        return df.at[id_loja, coluna]
+    return 0
+    
 # ========
 # MAIN
 # ========
 
 def main():
-    df = carregar_dados(ARQUIVO_DADOS)
+    df_vendas, df_emails_lojas = carregar_dados(
+        ARQUIVO_VENDAS,
+        ARQUIVO_EMAILS,
+        ARQUIVO_LOJAS
+    )
 
-    faturamento_dia, faturamento_ano = calcular_faturamento(df)
-    variedade_itens_dia, variedade_itens_ano = calcular_variedade(df)
-    ticket_medio_dia, ticket_medio_ano = calcular_ticket_medio(df)
+    faturamento_dia, faturamento_ano = calcular_faturamento(df_vendas)
+    variedade_itens_dia, variedade_itens_ano = calcular_variedade(df_vendas)
+    ticket_medio_dia, ticket_medio_ano = calcular_ticket_medio(df_vendas)
     
     lista_dfs = [
         (faturamento_dia, "Faturamento Diario", FAT_META_DIA),
@@ -122,6 +166,139 @@ def main():
         ticket_medio_dia,
         ticket_medio_ano
     ) = dfs_novos
+
+    data_mais_recente, ano_atual = data_atual(df_vendas)
+
+    lista_dfs_dia = (
+        faturamento_dia,
+        variedade_itens_dia,
+        ticket_medio_dia,
+    )
+
+    dfs_filtrados_dia = [
+        filtrar_data_dfs(df, "Data", data_mais_recente).set_index("ID Loja")
+        for df in lista_dfs_dia
+    ]
+
+    (
+        faturamento_dia_fil,
+        variedade_dia_fil,
+        ticket_dia_fil,
+    ) = dfs_filtrados_dia
+
+
+    lista_dfs_ano = (
+        faturamento_ano,
+        variedade_itens_ano,
+        ticket_medio_ano
+    )
+
+    dfs_filtrados_ano = [
+        filtrar_data_dfs(df, "Ano", ano_atual).set_index("ID Loja")
+        for df in lista_dfs_ano
+    ]
+
+    (
+        faturamento_ano_fil,
+        variedade_ano_fil,
+        ticket_ano_fil,
+    ) = dfs_filtrados_ano
+
+
+    for id_loja in df_emails_lojas["ID Loja"].dropna().unique():
+        onepage_dia = pd.DataFrame({
+            "Indicador": [
+                "Faturamento",
+                "Variedade",
+                "Ticket Medio"
+            ],
+            "Valor Dia": [
+                loc_onepage(
+                    faturamento_dia_fil,
+                    id_loja,
+                    "Faturamento Diario",
+                ),
+                loc_onepage(
+                    variedade_dia_fil,
+                    id_loja,
+                    "Variedade Diaria",
+                ),
+                loc_onepage(
+                    ticket_dia_fil,
+                    id_loja,
+                    "Ticket Medio Dia",
+                )
+            ],
+            "Meta Dia": [
+                FAT_META_DIA,
+                VAR_META_DIA,
+                TICKET_META
+            ],
+            "Cenário Dia": [
+                loc_onepage(
+                    faturamento_dia_fil,
+                    id_loja,
+                    "Bateu Meta",
+                ),
+                loc_onepage(
+                    variedade_dia_fil,
+                    id_loja,
+                    "Bateu Meta",
+                ),
+                loc_onepage(
+                    ticket_dia_fil,
+                    id_loja,
+                    "Bateu Meta",
+                )
+            ]       
+        }).set_index("Indicador")
+
+        onepage_ano = pd.DataFrame({
+            "Indicador": [
+                "Faturamento",
+                "Variedade",
+                "Ticket Medio"
+            ],
+            "Valor Ano": [
+                loc_onepage(
+                    faturamento_ano_fil,
+                    id_loja,
+                    "Faturamento Anual",
+                ),
+                loc_onepage(
+                    variedade_ano_fil,
+                    id_loja,
+                    "Variedade Anual",
+                ),
+                loc_onepage(
+                    ticket_ano_fil,
+                    id_loja,
+                    "Ticket Medio Ano",
+                )
+            ],
+            "Meta Ano": [
+                FAT_META_ANO,
+                VAR_META_ANO,
+                TICKET_META
+            ],
+            "Cenário Ano": [
+                loc_onepage(
+                    faturamento_ano_fil,
+                    id_loja,
+                    "Bateu Meta",
+                ),
+                loc_onepage(
+                    variedade_ano_fil,
+                    id_loja,
+                    "Bateu Meta",
+                ),
+                loc_onepage(
+                    ticket_ano_fil,
+                    id_loja,
+                    "Bateu Meta",
+                )
+            ]       
+        }).set_index("Indicador")
 
     print("Processamento concluído")
 
